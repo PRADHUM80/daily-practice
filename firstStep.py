3f5f5bb6cca2ba8647431978b0c4b4fcb558a8c5
Pradhum80 <kumarroney154@gmail.com>
2026-01-16 22:39:31 +0530

    first Step

---
+++ firstStep.py
@@ -0,0 +1,11 @@
+# Printing on the Same Line..
+print("Hello," , end=" ")
+print("world!")
+
+# Printing with Special Characters: New Line And Tabs..
+print("Hello, Python!\nWelcome to the world of coding.")
+
+print("Name:\tPradhum")
+print("Age:\t21")
+print("City:\tBangalore")
+print("Course:\tPython Programming")
