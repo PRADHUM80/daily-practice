2c1d8da3342e582523f0edad1349a0a2f40cc2ce
Pradhum80 <kumarroney154@gmail.com>
2026-01-16 23:31:27 +0530

    Intro : java

---
+++ JAVA/intro1.java
@@ -0,0 +1,14 @@
+// java is a programming language that is used to build apps, website, and software.
+// it is simple , secure, and platform-independent language. 
+
+
+
+package JAVA;
+
+public class intro1 {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
+    
+}
