ccb3f81d2da27613a5ac686d61991edd4b3a520b
Pradhum80 <kumarroney154@gmail.com>
2026-01-16 22:57:19 +0530

    Variables : in Python

---
+++ Variables3.py
@@ -0,0 +1,35 @@
+# Variables: Storing and Usoing Data.
+
+# Assigning Values to Variables..
+#   height = 180
+#  city = "Bangalore"
+sum_result = 10 + 5
+print(sum_result)
+
+
+
+# Changing Variable Values..
+score = 0
+print("Initial score:", score)
+# Updating The Score..
+score = score + 10
+print("Updated score:", score)
+
+
+
+# Using Variables in expression..
+width = 5
+height = 10
+area = width * height
+print("The area is:", area)
+
+
+# Combining Variable with Text..
+name = "Pradhum"
+age = 21
+print("Hello, my name is " + name + " and i am " + str(age) + "years old.")
+
+
+# Variables Types and Dynamic Typing..
+x = 10
+x = "Hello"
