d03f7509f5759298c06ad9638a6ffe4750566faf
Pradhum80 <kumarroney154@gmail.com>
2026-01-22 15:42:21 +0530

    Run: PolymerPhism

---
+++ JAVA/RunPolymer19.java
@@ -0,0 +1,31 @@
+package JAVA;
+
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+public class RunPolymer19 {
+       public static void main(String[] args) {
+
+        Animal a;
+
+        a = new Dog();
+        a.sound();   // Dog barks
+
+        a = new Cat();
+        a.sound();   // Cat meows
+    }
+}
